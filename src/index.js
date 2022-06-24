af5d302d14c2de1f6f957c8f4367486493296f22
Anastasiia Bandurova <banduova.nastya@gmail.com>
2022-06-24 13:17:37 -0700

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+    let reverseNumber = Math.abs(n)
+                        .toString()
+                        .split('')
+                        .reverse()
+                        .join('');
+    return Number(reverseNumber);
+};
